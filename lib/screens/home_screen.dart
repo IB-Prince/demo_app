44faf00d4a0b488cf44e02c5025b0a26e7f2257d
IB Prince <ibprince48@gmail.com>
2022-12-08 18:04:31 +0600

    updated home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -35,6 +35,9 @@ class _HomeScreenState extends State<HomeScreen> {
                           MaterialPageRoute(
                               builder: (context) => DescriptionPage(
                                     appBarTitle: 'Black Panther',
+                                movie_name: 'Black Panther',
+                                image_asset: 'black-panther.jpg',
+                                release_year: '2018',
                                   )));
                     },
                   ),
@@ -51,6 +54,9 @@ class _HomeScreenState extends State<HomeScreen> {
                           MaterialPageRoute(
                               builder: (context) => DescriptionPage(
                                     appBarTitle: 'Beauty and The Beast',
+                                image_asset: 'Beauty-Beast.jpg',
+                                movie_name: 'Beauty and The Beast',
+                                release_year: '2017',
                                   )));
                     },
                   ),
@@ -67,7 +73,71 @@ class _HomeScreenState extends State<HomeScreen> {
                           MaterialPageRoute(
                               builder: (context) => DescriptionPage(
                                     appBarTitle: 'Captain Marvel',
+                                image_asset: 'captain_marvel.jpg',
+                                movie_name: 'Captain Marvel',
+                                release_year: '2018',
                                   )));
+                    },
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(
+                    cardChild: PosterContent(
+                      image_asset: 'thor_dark_world.jpg',
+                      movie_name: 'Thor: The Dark World-2013',
+                    ),
+                    onPress: () {
+                      Navigator.push(context, MaterialPageRoute(
+                        builder: (context) => DescriptionPage(
+                            appBarTitle: 'Thor: The Dark WOrld',
+                            image_asset: 'thor_dark_world.jpg',
+                            movie_name: 'Thor: The Dark World',
+                            release_year: '2013',
+                        ),
+                      ),
+                      );
+                    },
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    cardChild: PosterContent(
+                      image_asset: 'thor_ragnarok.jpg',
+                      movie_name: 'Thor: Ragnarok-2017',
+                    ),
+                    onPress: () {
+                      Navigator.push(context, MaterialPageRoute(
+                        builder: (context) => DescriptionPage(
+                          appBarTitle: 'Thor: Ragnarok',
+                          image_asset: 'thor_ragnarok.jpg',
+                          movie_name: 'Thor: Ragnarok',
+                          release_year: '2017',
+                        ),
+                      ),
+                      );
+                    },
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    cardChild: PosterContent(
+                      image_asset: 'iron_man.jpg',
+                      movie_name: 'Iron Man',
+                    ),
+                    onPress: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => DescriptionPage(
+                                appBarTitle: 'Iron Man',
+                                movie_name: 'Iron Man',
+                                image_asset: 'iron_man.jpg',
+                                release_year: '2008',
+                              )));
                     },
                   ),
                 ),
@@ -81,25 +151,55 @@ class _HomeScreenState extends State<HomeScreen> {
                       image_asset: 'black-panther.jpg',
                       movie_name: 'Black Panther',
                     ),
-                    onPress: () {},
+                    onPress: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => DescriptionPage(
+                                appBarTitle: 'Black Panther',
+                                movie_name: 'Black Panther',
+                                image_asset: 'black-panther.jpg',
+                                release_year: '2018',
+                              )));
+                    },
                   ),
                 ),
                 Expanded(
                   child: ReusableCard(
                     cardChild: PosterContent(
-                      image_asset: 'black-panther.jpg',
-                      movie_name: 'Black Panther',
+                      image_asset: 'Beauty-Beast.jpg',
+                      movie_name: 'Beauty and The Beast',
                     ),
-                    onPress: () {},
+                    onPress: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => DescriptionPage(
+                                appBarTitle: 'Beauty and The Beast',
+                                image_asset: 'Beauty-Beast.jpg',
+                                movie_name: 'Beauty and The Beast',
+                                release_year: '2017',
+                              )));
+                    },
                   ),
                 ),
                 Expanded(
                   child: ReusableCard(
                     cardChild: PosterContent(
-                      image_asset: 'black-panther.jpg',
-                      movie_name: 'Black Panther',
+                      image_asset: 'captain_marvel.jpg',
+                      movie_name: 'Captain Marvel',
                     ),
-                    onPress: () {},
+                    onPress: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => DescriptionPage(
+                                appBarTitle: 'Captain Marvel',
+                                image_asset: 'captain_marvel.jpg',
+                                movie_name: 'Captain Marvel',
+                                release_year: '2018',
+                              )));
+                    },
                   ),
                 ),
               ],
@@ -109,59 +209,60 @@ class _HomeScreenState extends State<HomeScreen> {
                 Expanded(
                   child: ReusableCard(
                     cardChild: PosterContent(
-                      image_asset: 'black-panther.jpg',
-                      movie_name: 'Black Panther',
+                      image_asset: 'thor_dark_world.jpg',
+                      movie_name: 'Thor: The Dark World-2013',
                     ),
-                    onPress: () {},
+                    onPress: () {
+                      Navigator.push(context, MaterialPageRoute(
+                        builder: (context) => DescriptionPage(
+                          appBarTitle: 'Thor: The Dark WOrld',
+                          image_asset: 'thor_dark_world.jpg',
+                          movie_name: 'Thor: The Dark World',
+                          release_year: '2013',
+                        ),
+                      ),
+                      );
+                    },
                   ),
                 ),
                 Expanded(
                   child: ReusableCard(
                     cardChild: PosterContent(
-                      image_asset: 'black-panther.jpg',
-                      movie_name: 'Black Panther',
+                      image_asset: 'thor_ragnarok.jpg',
+                      movie_name: 'Thor: Ragnarok-2017',
                     ),
-                    onPress: () {},
+                    onPress: () {
+                      Navigator.push(context, MaterialPageRoute(
+                        builder: (context) => DescriptionPage(
+                          appBarTitle: 'Thor: Ragnarok',
+                          image_asset: 'thor_ragnarok.jpg',
+                          movie_name: 'Thor: Ragnarok',
+                          release_year: '2017',
+                        ),
+                      ),
+                      );
+                    },
                   ),
                 ),
                 Expanded(
                   child: ReusableCard(
                     cardChild: PosterContent(
-                      image_asset: 'black-panther.jpg',
-                      movie_name: 'Black Panther',
+                      image_asset: 'iron_man.jpg',
+                      movie_name: 'Iron Man',
                     ),
-                    onPress: () {},
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: ReusableCard(
-                    cardChild: PosterContent(
-                      image_asset: 'black-panther.jpg',
-                      movie_name: 'Black Panther',
-                    ),
-                    onPress: () {},
-                  ),
-                ),
-                Expanded(
-                  child: ReusableCard(
-                    cardChild: PosterContent(
-                      image_asset: 'black-panther.jpg',
-                      movie_name: 'Black Panther',
-                    ),
-                    onPress: () {},
-                  ),
-                ),
-                Expanded(
-                  child: ReusableCard(
-                    cardChild: PosterContent(
-                      image_asset: 'black-panther.jpg',
-                      movie_name: 'Black Panther',
-                    ),
-                    onPress: () {},
+                    onPress: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => DescriptionPage(
+                                appBarTitle: 'Iron Man',
+                                movie_name: 'Iron Man',
+                                image_asset: 'iron_man.jpg',
+                                release_year: '2008',
+                              ),
+                          ),
+                      );
+                    },
                   ),
                 ),
               ],
