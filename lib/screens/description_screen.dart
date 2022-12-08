274dc42ab3a00d5123e7f5bcd9f28759accae602
IB Prince <ibprince48@gmail.com>
2022-12-08 18:02:59 +0600

    modified

--- lib/screens/description_screen.dart
+++ lib/screens/description_screen.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 
 class DescriptionPage extends StatelessWidget {
-  DescriptionPage({required this.appBarTitle});
+  DescriptionPage({required this.appBarTitle, required this.image_asset, required this.movie_name, required this.release_year});
 
   final String appBarTitle;
+  final String image_asset;
+  final String movie_name;
+  final String release_year;
 
   @override
   Widget build(BuildContext context) {
@@ -12,11 +15,41 @@ class DescriptionPage extends StatelessWidget {
         title: Text(appBarTitle),
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text('Go back!'),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Image.asset('images/$image_asset', width: 100.0,height: 250.0),
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text('$movie_name'),
+                      SizedBox(height: 10.0),
+                      Text('$release_year'),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 10.0,),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Go back!'),
+            ),
+          ],
         ),
       ),
     );
